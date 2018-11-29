34d83947f719d05779ec1c70bc6cd5be56dff043
arthur <arthurckao@gmail.com>
2018-11-29 11:16:45 +0800

    Exports bootstrapped express app from setup/express.js

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
 const _debug = require('debug');
-
 const debug = _debug('init');
 const PORT = process.env.PORT || 3000;
-const serverUtil = require('./setup/express');
 const dbUtil = require('./setup/mongoose');
-
-const app = express();
+const app = require('./setup/express');
 
 // ***** setup/connect mongoose *****
-dbUtil.init()
-  .then(() => {
-    // ***** bring up express *****
-    serverUtil.middleWare(app);
-    serverUtil.routes(app);
-    serverUtil.errorHandlers(app);
-    app.listen(PORT, () => debug('express server listening on port ' + PORT));
-  }, (err) => debug('error establishing mongodb connection'))
-  .catch(() => debug('error establishing express api server'));
+dbUtil.init().then(() => {
+  // ***** start up express *****
+  app.listen(PORT, () => debug('express server listening on port ' + PORT));
+});
 
 
-
-
-//TODO /api doc (swagger)
-//test: controller unit test(do not unit test route handler)
-//TODO test: service unit test (research no DB access?)
-//TODO test: api level integration test (supertest)
-//1. public: root undefined healthcheck
-//2. auth: login
-//3. user: crud
+//TODO /api doc (swagger) => readme.md
+//TODO test: util unit test
+//TODO test: integration test
--- setup/express.js
+++ setup/express.js
@@ -1,5 +1,6 @@
 const _debug = require('debug');
 const morgan = require('morgan');
+const express = require('express');
 const bodyParser = require('body-parser');
 const compress = require('compression');
 const methodOverride = require('method-override');
@@ -12,78 +13,100 @@ const route = require('../route');
 const parseReqQueryObject = require('../route/middleware/parseReqQuery');
 const transformRASimpleRest = require('../route/middleware/transformRASimpleRest');
 
+// grab a "clean" express app
+const app = express();
+
 const corsOpts = {
-  exposedHeaders: ['Content-Range']
+  exposedHeaders: ['Content-Range']  //for ra-simple-rest api client
 };
 
-module.exports = {
-  middleWare: (app) => {
-    debug('setup middleware');
-    app.use(morgan('dev', {
-      skip: function (req, res) {
-        return res.statusCode < 400
-      }, stream: process.stderr
-    }));
-
-    app.use(morgan('dev', {
-      skip: function (req, res) {
-        return res.statusCode >= 400
-      }, stream: process.stdout
-    }));
-    // parse body params and attache them to req.body
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(compress());
-    app.use(methodOverride());
-
-    // secure apps by setting various HTTP headers
-    app.use(helmet());
-
-    // enable CORS - Cross Origin Resource Sharing
-    app.use(cors(corsOpts));
-
-    // JSON.parse req.query
-    app.use(parseReqQueryObject);
-
-    // client specific middleware
-    app.use(transformRASimpleRest);
-
-  },
-  routes: (app) => {
-    debug('setup routes');
-    // setup /api routes
-    app.use('/api', route);
-
-    // setup other (static) routes
-    app.get('/', (req, res) => res.send('Express-Mongoose-Rest-Api'));
-
-    // the catch all route
-    app.all('*', (req, res) => res.status(404).send('Page Not Found'));
-  },
-  errorHandlers: (app) => {
-    debug('setup custom error handlers');
-    // express-jwt error handler
-    app.use((err, req, res, next) => {
-      if(err.name === 'UnauthorizedError'){
-        _debug('app:error')('handling express-jwt error...');
-        return next(Boom.unauthorized('protected route'));
-      }
-      return next(err);
-    });
-    // boom error handler
-    app.use((err, req, res, next) => {
-      const debug = _debug('app:error');
-      debug('boom error handler invoked');
-      if(Boom.isBoom(err)){
-        debug('handling boom error...');
-        if (err.isServer) {
-          // log the error...
-          debug('err.message: ', err.message);
-        }
-        return res.status(err.output.statusCode).json(err.output.payload);
-      }
-      // let default error handler manage other non-api errors
-      return next(err);
-    });
+const setupMorgan = (app, env = 'dev') => {
+  const logErrorToStderr = morgan('dev', {
+    skip: function (req, res) {
+      return res.statusCode < 400
+    }, stream: process.stderr
+  });
+  const logToStdout = morgan('dev', {
+    skip: function (req, res) {
+      return res.statusCode >= 400
+    }, stream: process.stdout
+  });
+  switch(env) {
+    case 'dev':
+      app.use(logErrorToStderr);
+      app.use(logToStdout);
+      break;
+    case 'test':
+    case 'prod':
+    default:
   }
 };
+
+function middlewares(app) {
+  debug('setup middleware');
+
+  setupMorgan(app, process.env.NODE_ENV);
+  // parse body params and attache them to req.body
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(compress());
+  app.use(methodOverride());
+
+  // secure apps by setting various HTTP headers
+  app.use(helmet());
+
+  // enable CORS - Cross Origin Resource Sharing
+  app.use(cors(corsOpts));
+
+  // JSON.parse req.query
+  app.use(parseReqQueryObject);
+
+  // client specific middleware
+  app.use(transformRASimpleRest);
+
+}
+
+function routes(app) {
+  debug('setup routes');
+  // setup /api routes
+  app.use('/api', route);
+
+  // setup other (static) routes
+  app.get('/', (req, res) => res.send(process.env.APP_NAME || 'Express-Mongoose-Rest-API'));
+
+  // the catch all route
+  app.all('*', (req, res) => res.status(404).send('Page Not Found'));
+}
+
+function errorHandlers(app) {
+  debug('setup custom error handlers');
+  // express-jwt error handler
+  app.use((err, req, res, next) => {
+    if(err.name === 'UnauthorizedError'){
+      _debug('app:error')('handling express-jwt error...');
+      return next(Boom.unauthorized('protected route'));
+    }
+    return next(err);
+  });
+  // boom error handler
+  app.use((err, req, res, next) => {
+    const debug = _debug('app:error');
+    debug('boom error handler invoked');
+    if(Boom.isBoom(err)){
+      debug('handling boom error...');
+      if (err.isServer) {
+        // log the error...
+        debug('err.message: ', err.message);
+      }
+      return res.status(err.output.statusCode).json(err.output.payload);
+    }
+    // let default error handler manage other non-api errors
+    return next(err);
+  });
+}
+
+// inverse of control: pass app into those app-enhancers
+middlewares(app);
+routes(app);
+errorHandlers(app);
+module.exports = app;
