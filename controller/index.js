3798c8a03a8bdcefbe3ff5a7cdf420bc791e8047
arthur <arthurckao@gmail.com>
2018-12-14 18:04:42 +0800

    Export all modules defined in controller/service dir in index.js. Register all mongoose schemas defined in schema dir.

--- controller/index.js
+++ controller/index.js
@@ -5,10 +5,22 @@
  * It is the interface between http (outer facing) API and data service (inner facing) API
  * Route exits controller layer via res.status(...).json.(...) or Boom http error is thrown when exception occurs.
  * No database access in this layer.
+ *
+ * exports all the controllers defined in this dir, e.g.
+ * {
+ *   AuthController,
+ *   UserController
+ * }
  */
-const UserController = require('./user');
-const AuthController = require('./auth');
-module.exports = {
-  AuthController,
-  UserController
-};
+let obj = {};
+require('fs').readdirSync(__dirname).forEach((file) => {
+  const filePath = __dirname + '/' + file;
+  let property;
+
+  // exclude this index.js file and include other *.js files
+  if(filePath.indexOf('index') === -1 && filePath.indexOf('.js') !== -1){
+    property = file.charAt(0).toUpperCase() + file.substring(1, file.length-3) + 'Controller';
+    obj[property] = require(filePath);
+  }
+});
+module.exports = obj;
--- service/index.js
+++ service/index.js
@@ -1,9 +1,21 @@
-const UserService = require('./user');
-
 /**
  * Model layer:
  * Model in MVC convention where business logic is defined.
+ *
+ * exports all the services defined in this dir, e.g.
+ * {
+ *   UserService
+ * }
  */
-module.exports = {
-  UserService
-};
+let obj = {};
+require('fs').readdirSync(__dirname).forEach((file) => {
+  const filePath = __dirname + '/' + file;
+  let property;
+
+  // exclude this index.js file and include other *.js files
+  if(filePath.indexOf('index') === -1 && filePath.indexOf('.js') !== -1){
+    property = file.charAt(0).toUpperCase() + file.substring(1, file.length-3) + 'Service';
+    obj[property] = require(filePath);
+  }
+});
+module.exports = obj;
