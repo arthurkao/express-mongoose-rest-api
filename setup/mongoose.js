f3d3191e2aa7129c85bb6a939ab538c0d7c4461b
arthur <arthurckao@gmail.com>
2020-08-29 11:05:47 +0800

    updated mongoose package dependency (to use newer mongodb driver). Ref #5

--- setup/mongoose.js
+++ setup/mongoose.js
@@ -9,8 +9,7 @@ const options = {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-  reconnectInterval: 500, // Reconnect every 500ms
+  useUnifiedTopology: true,
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
